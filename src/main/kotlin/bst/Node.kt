287566c8e172af480cdd56d96cb5488f821e433e
Kyle Hall <hallkd47@gmail.com>
2018-08-27 22:49:45 -0500

    implement equals method

--- src/main/kotlin/bst/Node.kt
+++ src/main/kotlin/bst/Node.kt
@@ -1,3 +1,8 @@
 package bst
 
-data class Node(val key: Int)
+class Node(val key: Int) {
+    override fun equals(other: Any?): Boolean {
+        other as Node
+        return key == other.key
+    }
+}
--- src/test/kotlin/bst/NodeTest.kt
+++ src/test/kotlin/bst/NodeTest.kt
@@ -1,2 +1,37 @@
 package bst
 
+import org.junit.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class NodeTests {
+
+    @Test
+    fun equalsShouldReturnFalseWhenKeysAreDifferent() {
+        val n = Node(1)
+        val otherNode = Node(2)
+
+        assertFalse(n == otherNode)
+    }
+
+    @Test
+    fun equalsShouldReturnTrueWhenKeysAreSame() {
+        val n = Node(1)
+        val otherNode = Node(1)
+
+        assertTrue(n == otherNode)
+    }
+
+    @Test
+    fun equalsShouldThrowExceptionWhenOtherObjectIsNotANode() {
+        val n = Node(1)
+        val other = 1
+
+        assertFailsWith(
+                exceptionClass = ClassCastException::class,
+                block = { n.equals(other) }
+        )
+    }
+
+}
